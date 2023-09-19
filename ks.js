d953c13eabd3d8380357b849e3763f50f288cfcd
hekkojustajs <145416606+hekkojustajs@users.noreply.github.com>
2023-09-19 12:41:48 +0800

    Update ks.js

--- ks.js
+++ ks.js
@@ -1,5 +1,5 @@
 // test update
-
+/*
 $('body').css('background', '#FFF');
 $('body').css('display', 'none');
 
@@ -7,4 +7,4 @@ $('body').css('display', 'none');
 var test = document.getElementsByTagName('body');
 test[0].style.background = '#FFF';
 test[0].style.display = 'none';
-
+*/
