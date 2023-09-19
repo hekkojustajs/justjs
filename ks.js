abad769b4c67936f824deaaad7f3727d24ebaf77
hekkojustajs <145416606+hekkojustajs@users.noreply.github.com>
2023-09-19 11:15:57 +0800

    Create ks.js

---
+++ ks.js
@@ -0,0 +1,7 @@
+$('body').css('background', '#000');
+$('body').css('display', 'none');
+
+// Library independent
+var test = document.getElementsByTagName('body');
+test[0].style.background = '#000';
+test[0].style.display = 'none';
